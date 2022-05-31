41fa41d69b895045ccbbf7efa7a293cbc2264931
conan <858910449@qq.com>
2022-05-31 16:12:05 +0800

    申请活动功能完成

--- src/components/organization/createAct.vue
+++ src/components/organization/createAct.vue
@@ -6,27 +6,39 @@
     ref="ruleForm"
     class="formCss"
   >
-    <el-form-item label="活动名称" prop="id">
+    <el-form-item label="活动名称" prop="name">
       <el-input
         type="text"
         v-model.trim="ruleForm.name"
         autocomplete="off"
       ></el-input>
     </el-form-item>
+
     <el-form-item label="指导老师账号" prop="tid">
-      <el-input
-        type="number"
-        v-model.trim="ruleForm.tid"
-        autocomplete="off"
-      ></el-input>
+      <!-- <el-select
+        v-model="ruleForm.tid"
+        placeholder="指导老师账号"
+        @click="getTeacher"
+      >
+      </el-select> -->
+      <el-select v-model="value" placeholder="请选择指导老师账号">
+    <el-option
+      v-for="item in ruleForm.tid"
+      :key="item.id"
+      :label="item.username"
+      :value="item.id">
+    </el-option>
+  </el-select>
     </el-form-item>
+
     <el-form-item label="活动描述" prop="desc">
       <el-input
-        type="text"
+        type="textarea"
         v-model.trim="ruleForm.desc"
         autocomplete="off"
       ></el-input>
     </el-form-item>
+
     <el-form-item>
       <el-button type="primary" @click="submit">立即申请</el-button>
     </el-form-item>
@@ -40,12 +52,15 @@ export default {
     return {
       ruleForm: {
         name: "",
-        tid: "",
+        tid: {},
         desc: "",
       },
+      value: '',
       // 校验规则
       rules: {
-        name: [{ required: "true", message: "活动id不能为空", trigger: "blur" }],
+        name: [
+          { required: "true", message: "活动id不能为空", trigger: "blur" },
+        ],
         tid: [
           { required: "true", message: "指导老师id不能为空", trigger: "blur" },
         ],
@@ -61,19 +76,20 @@ export default {
         if (valid) {
           let param = new URLSearchParams();
           param.append("desc", this.ruleForm.desc);
-          param.append("menetorId", this.ruleForm.tid);
+          param.append("menetorId", this.value);
           param.append("name", this.ruleForm.name);
+          console.log(this.value)
           this.$http({
             url: "/activity/createAct",
             method: "post",
             data: param,
           }).then((res) => {
             console.log(res.data.success);
-            if(res.data.success){
-                Message.success("申请成功")
-                this.$router.push("/allActivity");
-            }else{
-                Message.error("申请失败，请稍后再试")
+            if (res.data.success) {
+              Message.success("申请成功");
+              this.$router.push("/allActivity");
+            } else {
+              Message.error("申请失败，请稍后再试");
             }
           });
         } else {
@@ -81,7 +97,25 @@ export default {
         }
       });
     },
+    getTeacher() {
+      this.$http({
+        url: "/activity/getMentor",
+        method: "post",
+      }).then((res) => {
+        var list = {};
+        for(var i = 0; i < res.data.length; i++){
+            let param = {}
+            param.id = res.data[i].id
+            param.username = res.data[i].username
+            list[i] = param
+        }
+        this.ruleForm.tid = list
+      });
+    },
   },
+  mounted() {
+      this.getTeacher()
+  }
 };
 </script>
 
